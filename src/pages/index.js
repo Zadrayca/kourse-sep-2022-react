1049787f678b4fbd4d951bdac9b56cafd83c5d3d
Zadrayca <coxatiy@gmail.com>
2023-02-09 20:26:09 +0200

    Homework6

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,4 @@ export * from './HomePage/HomePage';
 export * from './PostPage/PostPage';
 export * from './UsersPage/UsersPage';
 export * from './NotFoundPage/NotFoundPage';
-export * from './Cars/CarsPage';
+export * from './CarsPage/CarsPage';
