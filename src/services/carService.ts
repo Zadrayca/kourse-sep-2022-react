0d01060d373c86b0d16eda6ddb5bdb68a91fe2de
Zadrayca <coxatiy@gmail.com>
2023-02-17 15:59:11 +0200

    homework8 Cars TypeScript

--- src/services/carService.ts
+++ src/services/carService.ts
@@ -5,9 +5,9 @@ import {urls} from "../configs";
 const carService = {
     getAllCars: (): IRes<ICar[]> => apiService.get(urls.cars.base),
     getById: (id: string): IRes<ICar> => apiService.get(urls.cars.byId(id)),
-    create:(car:ICar): IRes<ICar> => apiService.post(urls.cars.base, car),
-    updateById:(id: string, data:ICar) => apiService.put(urls.cars.byId(id), data),
-    deleteById:(id: string) => apiService.delete(urls.cars.byId(id))
+    create: (car: ICar): IRes<ICar> => apiService.post(urls.cars.base, car),
+    updateById: (id: string, data: ICar): IRes<ICar> => apiService.put(urls.cars.byId(id), data),
+    deleteById: (id: string): IRes<void> => apiService.delete(urls.cars.byId(id))
 }
 
 export {
